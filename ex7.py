1ce866cc2bb8e90e026f5d650848d28c4f08599b
Tiffany Santos <47302131+TiffanySantos@users.noreply.github.com>
2019-06-05 10:21:14 +0200

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,22 @@
+print "Mary had a little lamb."
+print "Its fleace was white as %s." % 'snow' # the %s can be replaceable by any number of colours/ variables
+print "And everywhere that Mary went."
+print "." * 10 # what'd that do? - prints the string a set number of times, in this case a full-stop ten times
+# is this some sort of style thing? these appear to be just characters rahter than a string
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+# it makes the two character strings sit side by side instead of on seperate lines
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
